fdb4f2873ce3bc7b1d24b5f105d7a13bc73f50b8
Christina <Christinepeninsula@hotmail.com>
2019-03-02 11:50:26 -0500

    added recipe pictures

--- conf/settings.py
+++ conf/settings.py
@@ -151,6 +151,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/static/media')
 
 MEDIA_URL = '/media/'
